43d8033a49cd879db7275eadcdc7c55a21126319
Esteban Leivas <estebanleivas103@gmail.com>
2021-04-15 01:08:35 -0300

    Lang Updated

--- resources/lang/en/bot_messages.php
+++ resources/lang/en/bot_messages.php
@@ -16,9 +16,11 @@ return [
     'ashen_one' => 'Ashen One',
     'welcome' => "Welcome to the bonfire, :alias.\nI am a Fire Keeper.\nI tend to the flame, and tend to thee.\nThe Lords have left their thrones, and must be deliver'd to them.\nTo this end, I am at thy side.",
     'error' => "..:alias, it seems...\nsomething went wrong.",
+    'success' => "Very well.\nThen touch the darkness within me.\nTake nourishment from these sovereignless souls.\n...\nThou's request has been fulfilled, :alias.",
     'donate' => "Thou wish to pay respect to a Lord of Cinder?\nVery well.\nI tend to the flame, I tend to thee,\n.. and a long time ago, I tended to him.\nHere thou can place thy offerings.\n:donate_url",
     'change_language' => "Thou's language has changed?\nUnderstood.\nI tend to the flame, and I tend to thee.\nIf thou's voice speaks a foreign language\nmine should too.",
+    'change_alias' => "Thou's desire to change how I refer to thee, :alias?\nHow thou wishes to be called?",
     'remind' => "Very well.\nSay what thou wish to treasure.\nI shall remind thee of that.",
-    'remind_add' => "Very well.\nThen touch the darkness inside me.\nTake nourishment from these sovereignless souls.\nI shall remind thee about\n`:text_to_remind`.\nin :time_left.",
-    'remind_send' => ":alias.\nThou wished for me to remind thee \n`:text_to_remind`",
+    'remind_add' => "Very well.\nThen touch the darkness inside me.\nTake nourishment from these sovereignless souls.\nI shall remind thee about\n\":text_to_remind\".\nin :time_left.",
+    'remind_send' => ":alias.\nThou wished for me to remind thee \n\":text_to_remind\"",
 ];
